e549dec14a1d846b141749cc8614381d536cbf19
matkat100 <matiskino@gmail.com>
2025-01-07 15:19:16 +0200

    added nigga

--- chess_backEnd/Knight.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Piece.h"
-class Knight : public Piece
-{
-public:
-	Knight(const std::string& color);
-	std::string getColor() const override;
-	std::string getType() const override;
-};
--- chess_backEnd/Rook.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Piece.h"
-class Rook : public Piece
-{
-public:
-	Rook(const std::string& color);
-	std::string getColor() const override;
-	std::string getType() const override;
-};
